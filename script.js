1af0322024f62fb16d526c36db28bc6767f4fc44
Mykola2001 <108090444+Mykola2001@users.noreply.github.com>
2022-10-26 09:33:24 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+let number = prompt("Write some number here if you don't mind");
+
+let resoult;
+
+if (number = 0 || number <= -1 || number >= 1) {
+    resoult = 0 || resoult = 1 || resoult = -1
+} 
+const alertNumber = alert(resoult);
